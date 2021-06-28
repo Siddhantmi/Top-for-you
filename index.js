1f30f35ca4b928ee6ce5d740c0c1a9805b929728
siddhantmishra1-admin <siddhantmishrabusy949@gmail.com>
2021-06-28 11:35:21 +0530

    updated

--- index.js
+++ index.js
@@ -27,3 +27,19 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
